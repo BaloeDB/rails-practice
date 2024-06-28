5f01de7b19aaa218590b2a47792cfed91690c345
Peter Diepstraten <psrdiepstraten@gmail.com>
2024-06-28 20:09:05 +0200

    Exercise 18

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,9 @@ class Post < ApplicationRecord
     # VALIDATIONS
     validates :title, :content, presence: true 
     validates :title, :content, uniqueness: true
+
+    # SCOPES
+    # retrieve all posts with a specific `title`.
+    scope :contains_title, -> (title) { where("title LIKE ?",
+    sanitize_sql_like(title) + "%") }
 end
