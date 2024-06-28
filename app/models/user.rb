da4c149f82cfa21e21ae4604485ca8968b0b9eb2
Peter Diepstraten <psrdiepstraten@gmail.com>
2024-06-28 19:01:05 +0200

    Exercise 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+    # VALIDATIONS
     validates :name, :username, :email, presence: true 
     validates :name, :username, :email, uniqueness: true
+
+    # CALLBACKS
+    before_safe -> (user) { user.email = user.email.downcase }
 end
