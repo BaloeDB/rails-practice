dc6546b42b350342f3553195784e2ddc4dc8219a
Peter Diepstraten <psrdiepstraten@gmail.com>
2024-06-29 16:05:35 +0200

    2. **Length Validation**

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@
 class User < ApplicationRecord
     # Add a validation to ensure that the `username` is present.
     validates :username, presence: true
+    # Add a validation to the `User` model to ensure that the `username` 
+    # is between 3 and 20 characters long.
+    validates :username, length: { in: 3..20 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,25 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  test "Test the validation by creating a user without a `username`." do
+  test "Test the presence validation by creating a user without a `username`." do
     assert_raises ( ActiveRecord::RecordInvalid ) {
       User.create!()
     }
+  end
+
+  test "Test the length validation with both valid and invalid usernames." do
+    # Invalid usernames
+    assert_raises ( ActiveRecord::RecordInvalid ) {
+      User.create!(username: "aa")
+    }
+    assert_raises ( ActiveRecord::RecordInvalid ) {
+      User.create!(username: "aabbccddeeffgghhiijjkkllmm")
+    }
+
+    # Valid usernames
+    user1 = User.create!(username: "aabb")
+    user2 = User.create!(username: "aabbccddeeff")
+    assert_includes User.all, user1
+    assert_includes User.all, user2
   end
 end
