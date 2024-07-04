1f454adfc5e8ccb6ff2fe55d135536d330d3f738
Peter Diepstraten <psrdiepstraten@gmail.com>
2024-07-04 13:17:51 +0200

    Styling logs + logs can only be read

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,5 +1,6 @@
+# Logs can only be read. They can not be created/updated/deleted.
 class LogsController < ApplicationController
-  before_action :set_log, only: %i[ show edit update destroy ]
+  before_action :set_log, only: [:show]
 
   # GET /logs
   def index
@@ -10,49 +11,9 @@ class LogsController < ApplicationController
   def show
   end
 
-  # GET /logs/new
-  def new
-    @log = Log.new
-  end
-
-  # GET /logs/1/edit
-  def edit
-  end
-
-  # POST /logs
-  def create
-    @log = Log.new(log_params)
-
-    if @log.save
-      redirect_to @log, notice: "Log was successfully created."
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /logs/1
-  def update
-    if @log.update(log_params)
-      redirect_to @log, notice: "Log was successfully updated.", status: :see_other
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /logs/1
-  def destroy
-    @log.destroy!
-    redirect_to logs_url, notice: "Log was successfully destroyed.", status: :see_other
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_log
       @log = Log.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def log_params
-      params.require(:log).permit(:content)
     end
 end
